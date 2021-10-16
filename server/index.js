3a2a4769cd21ae8f9439e9938e3d1d3e97dbc72e
Adetunji Ojekunle <adetunji@collective2.com>
2021-10-16 11:44:00 +0100

    create new route to display contact list

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const ContactModel = require("./models/Contact");
 
 app.use(express.json());
-app.use(cors); 
+app.use(cors()); 
 
 mongoose.connect(
   "mongodb+srv://admin:password12345@contacty.ldc9f.mongodb.net/contact?retryWrites=true&w=majority",
@@ -17,24 +17,37 @@ mongoose.connect(
 
 app.post("/insert", async (req, res) => {
     
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const email = req.body.email;
-
-  const contact = new ContactModel({
-    contactFirstname: firstName,
-    contactLastname: lastName,
-    contactEmail: email,
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+  
+    const contact = new ContactModel({
+      contactFirstname: firstName,
+      contactLastname: lastName,
+      contactEmail: email,
+    });
+  
+    res.send("Success");
+    try {
+      await contact.save();
+    } catch (err) {
+      console.log(err);
+    }
   });
 
-  res.send("Success");
-  try {
-    await contact.save();
-  } catch (err) {
-    console.log(err);
-  }
-});
+
+  app.get("/read", async (req, res) => {
+    
+    ContactModel.find({},(err, result)=>{
+        if(err){
+            res.send(err)
+        }
+
+        res.send(result);
+    });
+
+  });
 
 app.listen(3001, () => {
-  console.log("Server running on port 3001");
+  console.log("Server running on port 3001...");
 });
