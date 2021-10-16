fb7ce7d6a7998857f0ce420e8d85b9f4825c9f18
Adetunji Ojekunle <adetunji@collective2.com>
2021-10-16 09:21:08 +0100

    Fixed Cors errors by adding the cors module through npm

--- server/index.js
+++ server/index.js
@@ -1,38 +1,40 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
 
-const ContactModel = require("./models/Contact")
+const ContactModel = require("./models/Contact");
 
 app.use(express.json());
+app.use(cors); 
 
-mongoose.connect('mongodb+srv://admin:password12345@contacty.ldc9f.mongodb.net/contact?retryWrites=true&w=majority',{
+mongoose.connect(
+  "mongodb+srv://admin:password12345@contacty.ldc9f.mongodb.net/contact?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
+  }
+);
+
+app.post("/insert", async (req, res) => {
+    
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const contact = new ContactModel({
+    contactFirstname: firstName,
+    contactLastname: lastName,
+    contactEmail: email,
+  });
+
+  res.send("Success");
+  try {
+    await contact.save();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post('/insert', async (req, res)=>{
-
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
-
-
-
-    const contact = new ContactModel({
-        contactFirstname: firstName, 
-        contactLastname : lastName,
-        contactEmail : email,
-    });
-
-    res.send("Success");
-    try {
-        await contact.save();
-    }catch(err){
-        console.log(err);
-    }
-
-});
-
-app.listen(3001,()=>{
-    console.log('Server running on port 3001');
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
 });
