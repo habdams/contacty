824dcdc3540c94928c8470477df4a21633c4765a
Adetunji Ojekunle <adetunji@collective2.com>
2021-10-17 05:45:16 +0100

    fix for contact list not seeing env PORT value

--- client/src/components/ContactList/ContactList.js
+++ client/src/components/ContactList/ContactList.js
@@ -1,13 +1,16 @@
 import React, {useState,useEffect} from 'react';
 import Axios from 'axios';
 import "./ContactList.css";
+require('dotenv').config();
 
 export const ContactList= () =>{
 
     const [contactList, setContactList] = useState([]);
 
+    const port = process.env.REACT_APP_PORT || 5000 ;
+
     useEffect(()=>{
-        Axios.get("http://localhost:3001/read").then(   (response) => {
+        Axios.get(`http://localhost:${port}/read`).then(   (response) => {
                 console.log(response.data);
                 setContactList(response.data)
             });
