c9990aed60e0bbd7cf284e123e21c70d660895d4
Adetunji Ojekunle <adetunji@collective2.com>
2021-10-17 04:56:32 +0100

    exclude secret from App by adding to .env file

--- server/index.js
+++ server/index.js
@@ -1,53 +1,53 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require('dotenv').config();
 const app = express();
 
 const ContactModel = require("./models/Contact");
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.REACT_APP_ATLAS_URI;
 
 mongoose.connect(
-  "mongodb+srv://admin:password12345@contacty.ldc9f.mongodb.net/contact?retryWrites=true&w=majority",
+  uri,
   {
     useNewUrlParser: true,
   }
 );
 
 app.post("/insert", async (req, res) => {
-    
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
-  
-    const contact = new ContactModel({
-      contactFirstname: firstName,
-      contactLastname: lastName,
-      contactEmail: email,
-    });
-  
-    res.send("Success");
-    try {
-      await contact.save();
-    } catch (err) {
-      console.log(err);
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const contact = new ContactModel({
+    contactFirstname: firstName,
+    contactLastname: lastName,
+    contactEmail: email,
+  });
+
+  res.send("Success");
+  try {
+    await contact.save();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/read", async (req, res) => {
+  ContactModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
     }
+
+    res.send(result);
   });
+});
 
-
-  app.get("/read", async (req, res) => {
-    
-    ContactModel.find({},(err, result)=>{
-        if(err){
-            res.send(err)
-        }
-
-        res.send(result);
-    });
-
-  });
-
-app.listen(3001, () => {
-  console.log("Server running on port 3001...");
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
